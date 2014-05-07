c3581f30cb7fa12dd56cca06425ab19655b54209
LeeNuri <leenl1016@naver.com>
2014-05-08 03:01:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 	$("#play").click(function() {
 		$(this).toggleClass("state1");
-		$(this).toggleClass("state2");
+
 
 		$("#playSound")[0].load();
 		$("#playSound")[0].play();
+	});
+	$("#play").click(function(){
+		$(this).toggleClass("state2");
 	});
 });
