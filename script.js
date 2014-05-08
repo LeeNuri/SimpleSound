538ff9f6630587d64ad4bfb5f79223a4c665a4bf
LeeNuri <leenl1016@naver.com>
2014-05-08 22:24:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	    $('#play').css('width' ,  $(window).width() - 105 );
-    $('#play').css('height' ,  $(window).height() - 105 );
+	    $('#play').css('width' ,  $(window).width() - 10 );
+    $('#play').css('height' ,  $(window).height() - 10 );
     $(window).resize(function() {
-      $('#play').css('width' ,  $(window).width() - 105 );
-      $('#play').css('height' ,  $(window).height() - 105 );
+      $('#play').css('width' ,  $(window).width() - 10 );
+      $('#play').css('height' ,  $(window).height() - 10 );
     });
 	$("#play").click(function() {
 		$(this).toggleClass("state1");
